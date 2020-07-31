194c6d2574d61d0ecaa23d5b239a6fcfe4b9fba8
Davi Leal <lealm.davi@gmail.com>
2020-07-30 23:07:36 -0300

    feat: Prepare to Connect to api Details Page

--- mobile/src/pages/Details/index.tsx
+++ mobile/src/pages/Details/index.tsx
@@ -3,16 +3,29 @@ import { View, Text } from 'react-native';
 
 import Card from '../../components/card';
 
-const Person = {
-  name: "Davi Leal",
-  description: "Oficial",
-  picture: "https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-27.jpg"
-}
-
+const Person = [
+  {
+    name: "Davi Leal",
+    description: "Oficial",
+    picture: "https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-27.jpg",
+  },
+  {
+    name: "Davi Leal",
+    description: "Oficial",
+    picture: "https://icon-library.com/images/no-profile-pic-icon/no-profile-pic-icon-27.jpg",
+  }
+];
 const Details: React.FC = () => {
 
   return (
-    <Card data={Person} />
+    <View style={{ flex: 1, backgroundColor: '#2d2d2d', flexDirection: 'column' }}>
+      <View style={{ marginTop: 20 }}>
+        {Person.map(response => (
+          <Card data={response} />
+        ))}
+      </View>
+
+    </View>
   )
 }
 
